4858028d0c8263f71dd76b58e257985e0e20877b
Rafaela Romin <rafaromin17@gmail.com>
2023-12-07 20:18:46 -0300

    removing unnecessary files

--- DevFreela.Application/InputModel/CreateCommentInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevFreela.Application.InputModel
-{
-    public class CreateCommentInputModel
-    {
-        public string Content { get; set; }
-        public int IdProject { get; set; }
-        public int IdUser { get; set; }
-    }
-}
